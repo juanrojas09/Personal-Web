bd80ffb286d579fd2db962f9699b029afb271572
juanrojas09 <62706206+juanrojas09@users.noreply.github.com>
2021-11-10 17:48:58 -0300

    ultimos cambios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*var btn=document.getElementsByClassName("boton");
+btn.document.addEventListener('click',function () {
+    var cont=0
+    while(btn.click){
+        cont++;
+    }
+    console.log(cont);
 
+})*/
+
+const spans = document.querySelectorAll(".word span"); //DEFINO VARIABLE DE SPAN, SELECCIONO WORD Y SPAN
+
+spans.forEach((span, idx) => {  //RECORRO CON
+    span.addEventListener("click", (e) => {
+        e.target.classList.add("active");
+    });
+    span.addEventListener("animationend", (e) => {
+        e.target.classList.remove("active");
+    });
+
+    // Initial animation
+    setTimeout(() => {
+        span.classList.add("active");
+    }, 750 * (idx + 1));
+});
 
 
 
